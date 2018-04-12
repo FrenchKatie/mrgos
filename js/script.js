a2428800b7892f8eb60cfdf3f67b1af66d6be068
Katie elizabeth French <845855192@bun2-206-16.local>
2018-04-13 11:51:05 +1200

    sixth push

--- js/script.js
+++ js/script.js
@@ -12,4 +12,27 @@ $(document).ready(function(){
   //   $(".open").slideToggle();
   // });
 
+  // ONLY COMPLETE THIS POPUP IF YOU HAVE TIME
+  // $('#go').click(function(){go(50)});
+  // $('#ok').click(function(){go(500)});
+
+  //setTimeout(function(){go(50)},700);
+  //setTimeout(function(){go(500)},2000);
+
+  function go(nr) {
+    $('.message').toggleClass('comein');
+    $('.check').toggleClass('scaledown');
+    $('#go').fadeToggle(nr);
+  }
+
+  $(document).ready( function() {
+  var elm = $('#htop');
+          function spin( vl ) {
+            elm.val( parseInt( elm.val(), 10 ) + vl );
+          }
+
+          $('#increase').click( function() { spin( 1 );  } );
+          $('#decrease').click( function() { spin( -1 ); } );
+});
+
 });
