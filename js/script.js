17e6f8219af953d689f2ba393330aa041d5d5ff9
Katie elizabeth French <845855192@bun2-206-16.local>
2018-04-18 12:46:56 +1200

    Final Push

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
 $(document).ready(function(){
   $(".toggleMobNav").click(function(){
     $(".toggleSection").slideToggle();
-  })
+  });
   $(".menuItem").click(function(){
     $(".toggleSection").slideToggle();
-  })
-
-  $('#go').click(function(){go(50)});
-  $('#ok').click(function(){go(500)});
-
-  function go(nr) {
-    $('.bb').fadeToggle(200);
-    $('.message').toggleClass('comein');
-    $('.check').toggleClass('scaledown');
-    $('#go').fadeToggle(nr);
-  }
-
-  $(".removeBox").click( function() {
-    $(".removeBox").hide();
   });
-})
+
+  $("#go").click(function(){go(50)});
+  $("#ok").click(function(){go(500)});
+
+  function go(nr){
+    $(".bb").fadeToggle(200);
+    $(".message").toggleClass("comein");
+    $(".check").toggleClass("scaledown");
+    $("#go").fadeToggle(nr);
+  };
+
+
+});
